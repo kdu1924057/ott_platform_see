3289d3143e0b7f41061e14417e05d7c559de53d3
kdu <111514472+kdu1924057@users.noreply.github.com>
2024-03-09 23:14:26 +0900

    second comme=it

--- main/src/main/java/com/example/demo/MainApplication.java
+++ main/src/main/java/com/example/demo/MainApplication.java
@@ -2,10 +2,17 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
+@Controller
 public class MainApplication {
-
+	
+	@GetMapping("/")
+	public String index() {
+		return "Hello Wolrd";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(MainApplication.class, args);
 	}
