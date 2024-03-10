e87038658f786e2df5b1cbb8f8e3a7dd19bcf363
kdu <111514472+kdu1924057@users.noreply.github.com>
2024-03-10 19:56:59 +0900

    third commit

--- See/src/main/java/com/example/demo/SeeApplication.java
+++ See/src/main/java/com/example/demo/SeeApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@Controller
+@RestController
 public class SeeApplication {
 
 	@GetMapping("/")
