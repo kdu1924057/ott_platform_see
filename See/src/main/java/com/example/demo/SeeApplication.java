e5082bc3f2806da31ba5a0eb021789aeac7811da
park <qkrwjdgus1441@naver.com>
2024-03-12 21:53:29 +0900

    기본 로그인,회원가입,홈화면

--- See/src/main/java/com/example/demo/SeeApplication.java
+++ See/src/main/java/com/example/demo/SeeApplication.java
@@ -2,11 +2,20 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SeeApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(SeeApplication.class, args);
-    }
+	@GetMapping("/")
+	public String index() {
+		return "Hello World";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(SeeApplication.class, args);
+	}
+
 }
