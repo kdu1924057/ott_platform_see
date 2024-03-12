85694a5ce8ca79d10029e76b8fc1d297d36f27ed
park <qkrwjdgus1441@naver.com>
2024-03-12 14:02:05 +0900

    Merge branch 'OTT_platform_See_1' of https://github.com/kdu1924057/ott_platform_see.git

--- See/src/main/java/com/example/demo/SeeApplication.java
+++ See/src/main/java/com/example/demo/SeeApplication.java
@@ -2,20 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class SeeApplication {
 
-	@GetMapping("/")
-	public String index() {
-		return "Hello World";
-	}
-	public static void main(String[] args) {
-		SpringApplication.run(SeeApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(SeeApplication.class, args);
+    }
 }
